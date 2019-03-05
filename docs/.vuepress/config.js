46db67594e6f8a8540c44f4cf525e3bf28cd6307
yaimeet <yaimeet@126.com>
2019-03-05 18:30:59 +0800

    update docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+/**
+ * @author yaimeet
+ * @date 2019/3/5
+ * @Description
+ */
+module.exports = {
+    title: 'yaim-vue-design',
+    description: '基于Vue设计的开发模式',
+    themeConfig: {
+        lastUpdated: '2019-03-05',
+        nav: [
+            {
+                text: '教程',
+                link: '/guide/'
+            },
+            {
+                text: '参考链接',
+                items: [
+                    { text: 'Vue', link: 'https://cn.vuejs.org/' },
+                    { text: 'Vuex', link: 'https://vuex.vuejs.org/zh/' },
+                    { text: 'VueRouter', link: 'https://router.vuejs.org/zh/' }
+                ]
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/yaimeet/yaim-vue-design'
+            }
+        ],
+        sidebar: [
+            ['/', '首页'],
+            ['/use', '使用'],
+            ['/dev', '开发'],
+            ['/config', '配置']
+        ]
+    }
+}
